2cf0b88168e772a6c38c6a02a294a49443f3ddf4
Piotr Ginal <piotr.ginal@nokia.com>
2025-02-12 15:07:39 +0100

    feat: add ssh askpass settings

--- zsh_custom/env_variables.zsh
+++ zsh_custom/env_variables.zsh
@@ -7,3 +7,7 @@ export PIPX_DEFAULT_PYTHON="/home/ginal/.pyenv/versions/3.11.9/bin/python"
 # ---- nvim ----
 export PATH="$PATH:/opt/nvim-linux64/bin"
 export PATH="$PATH:/home/ginal/.local/nvim/usr/bin"
+
+# ---- ssh ----
+export SSH_ASKPASS_REQUIRE=force
+export SSH_ASKPASS=/usr/bin/systemd-ask-password
