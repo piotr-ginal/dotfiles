d5d6fb0748569fac398742328ce713c0b6e2e60f
Piotr Ginał <ginal.piotr11@gmail.com>
2025-03-12 17:57:06 +0100

    feat: add i3blocks mic script

---
+++ i3blocks/scripts/mic.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mic_mute_status=$(amixer get Capture | grep -E -o '\[on\]|\[off\]' | head -n 1)
+
+
+if [[ "$mic_mute_status" == "[off]" ]]; then
+    mic_status="muted"
+    bg_color="#565143"
+else
+    mic_status="on"
+    bg_color="#FF0000"
+fi
+
+echo "<span background='#232D38'> mic </span><span background='${bg_color}'> ${mic_status} </span>"
