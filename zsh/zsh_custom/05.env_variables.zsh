1433e7c2e00f5f1751db359cdeb86dca32635c63
Piotr Ginał <ginal.piotr11@gmail.com>
2025-05-02 13:56:04 +0200

    fix: remove hardcoded user in env variables

--- zsh/zsh_custom/05.env_variables.zsh
+++ zsh/zsh_custom/05.env_variables.zsh
@@ -2,11 +2,11 @@
 export PIP_DISABLE_PIP_VERSION_CHECK=1  # disables the annoying info about pip being upgradable
 
 # ---- pipx variables ----
-export PIPX_DEFAULT_PYTHON="/home/ginal/.pyenv/versions/3.11.9/bin/python"
+export PIPX_DEFAULT_PYTHON="/home/$(whoami)/.pyenv/versions/3.11.9/bin/python"
 
 # ---- nvim ----
 export PATH="$PATH:/opt/nvim-linux64/bin"
-export PATH="$PATH:/home/ginal/.local/nvim/usr/bin"
+export PATH="$PATH:/home/$(whoami)/.local/nvim/usr/bin"
 
 # ---- ssh ----
 export SSH_ASKPASS_REQUIRE=force
