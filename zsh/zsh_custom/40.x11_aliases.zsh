7910cdb5c1dabd455b00946a741db97269b1deee
Piotr Ginał <ginal.piotr11@gmail.com>
2025-05-02 13:57:25 +0200

    feat: add x11 alias for listing connected monitors

--- zsh/zsh_custom/40.x11_aliases.zsh
+++ zsh/zsh_custom/40.x11_aliases.zsh
@@ -1,6 +1,7 @@
-# overrides wayland only aliases
+# overrides wayland only aliases (and add new ones)
 if [[ "$XDG_SESSION_TYPE" == "x11" ]]; then
   alias copy="sed -z '$ s/\n$//' | xclip -selection clipboard"
   alias cppwd="pwd | copy"
   alias lock="i3lock -c 000000"
+  alias monitors='xrandr | grep " connected" | awk '\''{ print $1 }'\'
 fi
