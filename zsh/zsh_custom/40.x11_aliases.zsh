b2b88d13245085979986ad707a0f25570f43f89f
Piotr Ginał <ginal.piotr11@gmail.com>
2025-05-29 16:12:23 +0200

    fix: cpp alias workaround for x11
    i cant be bothered to put up with zsh shenanigans maybe ill fix this some day

--- zsh/zsh_custom/40.x11_aliases.zsh
+++ zsh/zsh_custom/40.x11_aliases.zsh
@@ -4,4 +4,6 @@ if [[ "$XDG_SESSION_TYPE" == "x11" ]]; then
   alias cppwd="pwd | copy"
   alias lock="i3lock -c 000000"
   alias monitors='xrandr | grep " connected" | awk '\''{ print $1 }'\'
+  unfunction cpp
+  cpp() { realpath "$@" | sed -z '$ s/\n$//' | xclip -selection clipboard; }
 fi
