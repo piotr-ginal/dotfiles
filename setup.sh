ec079c204c274289978b0b41d12ec633009837ca
Piotr Ginał <ginal.piotr11@gmail.com>
2025-10-07 16:41:09 +0200

    feat: add mako config

--- setup.sh
+++ setup.sh
@@ -64,7 +64,7 @@ fi
 [ -d "$HOME/.config" ] || mkdir "$HOME/.config"
 
 # ---- Create symlinks for config directories ----
-CONFIG_DIRS=("fuzzel" "i3blocks" "kitty" "sway" "alacritty" "eza" "git" "ptpython" "yazi" "tmux" "swaync" "tealdeer" "helix" "macchina" "waybar")
+CONFIG_DIRS=("fuzzel" "i3blocks" "kitty" "sway" "alacritty" "eza" "git" "ptpython" "yazi" "tmux" "swaync" "tealdeer" "helix" "macchina" "waybar" "mako")
 for config_dir in "${CONFIG_DIRS[@]}"; do
     TARGET="$HOME/.config/$config_dir"
     SOURCE="$(pwd)/$config_dir"
