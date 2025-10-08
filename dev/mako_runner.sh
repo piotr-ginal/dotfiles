9628d90f0f9196f38b1c2ecd5faa3271112753a4
Piotr Ginał <ginal.piotr11@gmail.com>
2025-10-08 02:15:36 +0200

    feat: add script for refreshing mako

---
+++ dev/mako_runner.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CONFIG_FILES="$HOME/.dotfiles/mako/config"
+
+while true; do
+    clear
+    makoctl history | tail -n 30
+    echo "waiting for configs to change"
+    inotifywait -e create,modify $CONFIG_FILES
+    echo "refresing mako"
+    makoctl reload
+    notify-send -t 2000 -u critical "crit app" -a "app"
+    notify-send -t 2000 -u normal "normal fire" -a "Firefox"
+done
