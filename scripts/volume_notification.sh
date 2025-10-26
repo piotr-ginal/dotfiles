b809c1b1e634448fbcf62775d37cd58c3b359ac9
Piotr Ginał <ginal.piotr11@gmail.com>
2025-10-26 12:31:16 +0100

    feat: add volume notification script
    volume still shown when muted as my keybinds allow volume to change when
    muted

---
+++ scripts/volume_notification.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+build_bar() {
+  local percent="$1"
+  local bar=""
+  local fill="⠿"
+  local empty="⠀"
+
+  for (( i=1; i<=20; i++ )); do
+    if (( i * 5 <= percent )); then
+      bar+="$fill"
+    else
+      bar+="$empty"
+    fi
+  done
+
+  printf '%s' "$bar"
+}
+
+volume_percentage=$(pactl get-sink-volume @DEFAULT_SINK@ | rg "(\d{1,3})%" -r '$1' -o | head -n1)
+
+mute_status=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}')
+
+if [ "$mute_status" = "yes" ]; then
+  mute_status_str=" (muted)"
+else
+  mute_status_str=""
+fi
+
+
+notify-send \
+  --app-name="volume-notify" \
+  --expire-time=1000 \
+  --print-id \
+  -h boolean:transient:true \
+  -h string:x-canonical-private-synchronous:osd \
+  "volume: $volume_percentage%$mute_status_str" "$(build_bar $volume_percentage)"
