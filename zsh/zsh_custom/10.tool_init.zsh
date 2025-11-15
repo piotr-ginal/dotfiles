ce98462e543bba6c5dcac60267165e2148a80dd7
Piotr Ginał <ginal.piotr11@gmail.com>
2025-11-15 20:28:06 +0100

    feat: remove pyenv load
    im using uv rn, pyenv init takes 200ms every time i open a new zsh
    instance, cant be having that around

--- zsh/zsh_custom/10.tool_init.zsh
+++ zsh/zsh_custom/10.tool_init.zsh
@@ -1,15 +1,5 @@
 # tools setup - does not include configuration
 
-# ---- pyenv ----
-export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-
-if command -v pyenv &>/dev/null; then
-  eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
-  export PATH=$(pyenv root)/shims:$PATH
-fi
-
 # ---- fzf (for zsh) ----
 command -v fzf &>/dev/null && source <(fzf --zsh)
 
