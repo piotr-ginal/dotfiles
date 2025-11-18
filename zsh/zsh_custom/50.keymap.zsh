f5d335722fb4341cd7f1828c3a54223f3ae35b71
Piotr Ginał <ginal.piotr11@gmail.com>
2025-11-18 01:22:57 +0100

    feat: shell keybinds for history and word movement

--- zsh/zsh_custom/50.keymap.zsh
+++ zsh/zsh_custom/50.keymap.zsh
@@ -1,1 +1,12 @@
 bindkey "^E" edit-command-line
+
+autoload -U up-line-or-beginning-search
+zle -N up-line-or-beginning-search
+bindkey -M emacs "^[[A" up-line-or-beginning-search
+
+autoload -U down-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey -M emacs "^[[B" down-line-or-beginning-search
+
+bindkey -M emacs '^[[1;5C' forward-word
+bindkey -M emacs '^[[1;5D' backward-word
