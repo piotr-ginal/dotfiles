e450a9e8bc8dc9355500457b0309ae5d7f6632df
Piotr Ginał <ginal.piotr11@gmail.com>
2025-11-20 23:57:12 +0100

    feat: dir aliases
    including d one i depend on in cdd

--- zsh/zsh_custom/20.aliases.zsh
+++ zsh/zsh_custom/20.aliases.zsh
@@ -362,9 +362,29 @@ alias dot="cd $DOTFILES_REPO_ROOT"
 alias rec="cd ~/.screenrecordings"
 alias scr="cd ~/.screenshots"
 
-alias ..="cd .."
-alias ...="cd ../.."
-alias ....="cd ../../.."
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
+alias -g ......='../../../../..'
+
+alias -- -='cd -'
+alias 1='cd -1'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -4'
+alias 5='cd -5'
+alias 6='cd -6'
+alias 7='cd -7'
+alias 8='cd -8'
+alias 9='cd -9'
+
+function d () {
+  if [[ -n $1 ]]; then
+    dirs "$@"
+  else
+    dirs -v | head -n 10
+  fi
+}
 
 alias cdl=cd_last_accessed_path
 
