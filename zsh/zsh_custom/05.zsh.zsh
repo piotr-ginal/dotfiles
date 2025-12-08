2f6e15d6d7c7098977677f4a6cfeda1fa9ecfdcf
Piotr Ginał <ginal.piotr11@gmail.com>
2025-12-08 22:20:10 +0100

    feat: general zsh configuration file
    history improvements
    
    cd qol improvements

---
+++ zsh/zsh_custom/05.zsh.zsh
@@ -0,0 +1,14 @@
+setopt auto_cd
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt pushdminus
+
+# ---- history ----
+setopt EXTENDED_HISTORY
+setopt INC_APPEND_HISTORY
+setopt SHARE_HISTORY
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_VERIFY
+setopt HIST_REDUCE_BLANKS
