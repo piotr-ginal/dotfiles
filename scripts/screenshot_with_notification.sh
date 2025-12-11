0277c6d80b32278ffe95a6712b3645f2c5fddbda
Piotr Ginał <ginal.piotr11@gmail.com>
2025-12-11 21:56:36 +0100

    feat: copy saved screenshot path to clipboard

--- scripts/screenshot_with_notification.sh
+++ scripts/screenshot_with_notification.sh
@@ -7,3 +7,5 @@ grimshot save area $output_filename &> /dev/null
 [ $? -ne 0 ] && exit 0
 
 notify-send -a screenshot-notification "Screenshot saved" $(basename $output_filename)
+
+wl-copy $output_filename
