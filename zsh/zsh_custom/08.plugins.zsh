6f39f088fc02de3c2b61e510c28a19037af93ec0
Piotr Ginał <ginal.piotr11@gmail.com>
2025-12-20 15:19:54 +0100

    feat: zcomet as zsh plugin manager
    with just zsh syntax highlighting avg shell startup improved compared to
    syntax highlighting from apt
    
    from
    
    39.4 ms ±   3.7 ms    [User: 30.2 ms, System: 12.2 ms]
    
    to
    
    34.8 ms ±   3.1 ms    [User: 26.9 ms, System: 11.0 ms]

---
+++ zsh/zsh_custom/08.plugins.zsh
@@ -0,0 +1,9 @@
+# ---- zcomet init ----
+if [[ ! -f ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh ]]; then
+  git clone https://github.com/agkozak/zcomet.git ${ZDOTDIR:-${HOME}}/.zcomet/bin
+fi
+
+source ${ZDOTDIR:-${HOME}}/.zcomet/bin/zcomet.zsh
+
+# ---- plugins ----
+zcomet load zsh-users/zsh-syntax-highlighting
