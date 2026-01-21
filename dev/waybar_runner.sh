b5f94a452488a0f0bb1c659ba18ba3f101148a5d
Piotr Ginał <ginal.piotr11@gmail.com>
2026-01-21 21:34:02 +0100

    feat: improve waybar refresh dev script
    - dont kill waybar use bultin config reload on SIGUSR2
    - ensure waybar is running before the reload (on each iteration)

--- dev/waybar_runner.sh
+++ dev/waybar_runner.sh
@@ -5,16 +5,11 @@ if ! type inotifywait &>/dev/null; then
   exit 1
 fi
 
-CONFIG_FILES="$DOTFILES_REPO_ROOT/waybar/config.jsonc $DOTFILES_REPO_ROOT/waybar/style.css $DOTFILES_REPO_ROOT/waybar/themes/white.css"
-
-trap "killall waybar" EXIT
-
 while true; do
-    clear
-    echo "starting waybar"
-    waybar &
-    echo "waiting for configs to change"
-    inotifywait -e create,modify $CONFIG_FILES
-    echo "killing all waybar instances"
-    killall waybar
+  pgrep -x waybar &>/dev/null || setsid waybar >/dev/null 2>&1 &
+  inotifywait -e create,modify -r "$DOTFILES_REPO_ROOT/waybar" 2>/dev/null || {
+      echo "failed to establish watch"
+      exit 1
+  }
+  killall -SIGUSR2 waybar
 done
