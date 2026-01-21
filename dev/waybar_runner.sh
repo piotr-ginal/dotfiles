93503035a87c50d385adccb13c27a90f70d6c3be
Piotr Ginał <ginal.piotr11@gmail.com>
2026-01-21 21:36:22 +0100

    feat: move waybar start after event on config detected

--- dev/waybar_runner.sh
+++ dev/waybar_runner.sh
@@ -6,10 +6,10 @@ if ! type inotifywait &>/dev/null; then
 fi
 
 while true; do
-  pgrep -x waybar &>/dev/null || setsid waybar >/dev/null 2>&1 &
   inotifywait -e create,modify -r "$DOTFILES_REPO_ROOT/waybar" 2>/dev/null || {
       echo "failed to establish watch"
       exit 1
   }
+  pgrep -x waybar &>/dev/null || setsid waybar >/dev/null 2>&1 &
   killall -SIGUSR2 waybar
 done
