1f86d1d6b18774fd0819801e5c91a88557d1dd4e
Piotr Ginał <ginal.piotr11@gmail.com>
2026-06-19 22:03:52 +0200

    feat: use keys from terminfo for zsh forward backward keymap

--- zsh/zsh_custom/50.keymap.zsh
+++ zsh/zsh_custom/50.keymap.zsh
@@ -1,3 +1,5 @@
+# man terminfo - key codes (kcuu1, kcud1 ...)
+# man zshcontrib - widgets used below
 bindkey -e
 
 autoload -U edit-command-line
@@ -6,13 +8,13 @@ bindkey "^E" edit-command-line
 
 autoload -U up-line-or-beginning-search
 zle -N up-line-or-beginning-search
-bindkey -M emacs "${terminfo[kcuu1]:-^[[A}" up-line-or-beginning-search
+bindkey -M emacs "${terminfo[kcuu1]:-^[[A}" up-line-or-beginning-search  # kcuu1: up-arrow key
 
 autoload -U down-line-or-beginning-search
 zle -N down-line-or-beginning-search
-bindkey -M emacs "${terminfo[kcud1]:-^[[B}" down-line-or-beginning-search
+bindkey -M emacs "${terminfo[kcud1]:-^[[B}" down-line-or-beginning-search  # kcud1: down-arrow key
 
-bindkey -M emacs '^[[1;5C' forward-word
-bindkey -M emacs '^[[1;5D' backward-word
+bindkey -M emacs "${terminfo[kcub1]:-^[[1;5C}" forward-word  # kcub1: left-arrow key
+bindkey -M emacs "${terminfo[kcuf1]:-^[[1;5D}" backward-word  # kcuf1: right-arrow key
 
 bindkey '^[[Z' reverse-menu-complete
