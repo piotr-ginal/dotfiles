fdbc0fb3e52a91536583ec3cb7c036485d1e64d4
Piotr Ginał <ginal.piotr11@gmail.com>
2026-07-22 17:29:01 +0200

    feat: calmer colors during window selection

--- scripts/screenshot_window.sh
+++ scripts/screenshot_window.sh
@@ -2,7 +2,7 @@
 
 geom="$(swaymsg -t get_tree \
   | jq -r '.. | select(.pid? and .visible?) | "\(.rect.x),\(.rect.y) \(.rect.width)x\(.rect.height)"' \
-  | slurp -r -c '#ff0000ff')" || exit 1
+  | slurp -b '#00000099' -B '#00000066' -w 3 -r -c '#bfbdb6ff')" || exit 1
 
 [ -z "$geom" ] && exit 1
 
