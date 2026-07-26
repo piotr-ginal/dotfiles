534536611bb952006d2eb48cd7f8faae93c43b19
Piotr Ginał <ginal.piotr11@gmail.com>
2026-07-27 01:46:04 +0200

    feat: include current output in geoms generation for slurp
    now you can screenshot the whole display

--- scripts/screenshot_window.sh
+++ scripts/screenshot_window.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 geom="$(swaymsg -t get_tree \
-  | jq -r '.. | select(.pid? and .visible?) | "\(.rect.x),\(.rect.y) \(.rect.width)x\(.rect.height)"' \
+  | jq -r '.. | select((.pid? and .visible?) or .type? == "output") | select(.name != "__i3") | "\(.rect.x),\(.rect.y) \(.rect.width)x\(.rect.height)"' \
   | slurp -b '#00000099' -B '#00000066' -w 3 -r -c '#bfbdb6ff')" || exit 1
 
 [ -z "$geom" ] && exit 1
