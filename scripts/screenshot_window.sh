3a417d1b571755d22676bba46cb2561a0b1479e0
Piotr Ginał <ginal.piotr11@gmail.com>
2026-07-27 01:49:32 +0200

    feat: copy saved screenshot path satty
    the output filename must be set as an arg because satty does not expand
    tildes
    
    therefore expand it in sh
    
    There is still a problem with this script, if we open the glob geom
    picker, and we change the workspace to one with a different layout (for
    example 3 open windows instead of 2) the old geoms will be shown on the
    screen - dont think this is too big of a problem

--- scripts/screenshot_window.sh
+++ scripts/screenshot_window.sh
@@ -6,4 +6,11 @@ geom="$(swaymsg -t get_tree \
 
 [ -z "$geom" ] && exit 1
 
-grim -g "$geom" - | satty --filename -
+grim -g "$geom" - \
+    | satty \
+        --filename - \
+        --output-filename "${HOME}/.screenshots/satty-%Y-%m-%d_%H:%M:%S.png" \
+    | while IFS= read -r line; do
+        path=$(printf '%s\n' "$line" | grep -oP "File saved to '\K[^']+(?=')")
+        [ -n "$path" ] && printf '%s' "$path" | wl-copy
+    done
